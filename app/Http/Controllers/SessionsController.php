aa52aa9adcbd1bc8313d11a3b09cef288292bf52
hseka <hseka@lescollectionneurs.com>
2021-10-07 02:02:56 +0200

    re-organisation de la méthode de login (connexion)

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -30,30 +30,28 @@ class SessionsController extends Controller
 
         // Attempt to authenticate and log in the User
         // based on the provided Credentials :
-        if(auth()->attempt($attributes)) {
+        if(! auth()->attempt($attributes)) {
 
-            // Se protéger de l'attaque "Session Fixation" :
-            session()->regenerate();
+            // Authentication failed :
+            throw ValidationException::withMessages([
+                'email' => 'Cet email n\'existe pas chez nous !'
+            ]);
 
-            // Redirect to Homepage with a success Flesh message :
-            return redirect('/')->with('success','Welcome back !');
+            /* Old méthode :
+            return back()
+                // les champs sont pré-ermplies des valeurs
+                // précédemment saisies :
+                ->withInput()
+                // Transfert du message d'erreur à la vue :
+                ->withErrors(['email' => 'Cet email n\'existe pas chez nous !' ]);
+            */
         }
 
-        /*
-        // Authentication failed :
-        return back()
-            // les champs sont pré-ermplies des valeurs
-            // précédemment saisies :
-            ->withInput()
-            // Transfert du message d'erreur à la vue :
-            ->withErrors(['email' => 'Cet email n\'existe pas chez nous !' ]);
-        */
+        // Se protéger de l'attaque "Session Fixation" :
+        session()->regenerate();
 
-        // 2ème méthode :
-        throw ValidationException::withMessages([
-            'email' => 'Cet email n\'existe pas chez nous !'
-        ])
-
+        // Redirect to Homepage with a success Flesh message :
+        return redirect('/')->with('success','Welcome back !');
 
     }
 
